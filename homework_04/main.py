bd46c6ee1208bd828e35ae324af3587c4b819e90
ckamone <ckamone@gmail.com>
2022-11-17 20:49:20 +0300

    some fixes

--- homework_04/main.py
+++ homework_04/main.py
@@ -13,7 +13,7 @@
 - закрытие соединения с БД
 """
 import asyncio
-from models import async_engine, Base, User, Post, Session
+from models import Base, User, Post, Session, async_engine
 from jsonplaceholder_requests import fetch_users_data, fetch_posts_data
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
@@ -27,6 +27,7 @@ async def create_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def create_user(session: AsyncSession, username: str, name: str, email: str) -> User:
     user = User(username=username, name=name, email=email)
@@ -42,15 +43,18 @@ async def async_main():
     await create_tables()
     users_data, posts_data = await asyncio.gather(fetch_users_data(), fetch_posts_data())
 
-    async with Session as session:
-        await create_user(session, "username", "name", "email")
+    async with Session() as conn:
+        await create_user(conn, username="username3", name="name3", email="email3")
+
         #for user_data in users_data:await create_user(Session, "username", "name", "email")
 
             #await create_user(session, user_data["username"], user_data["name"], user_data["email"])
 
 
 def main():
-    asyncio.run(async_main())
+    coro_1=async_main()
+    asyncio.run(coro_1)
+
 
 if __name__ == "__main__":
     main()
--- homework_04/models.py
+++ homework_04/models.py
@@ -23,16 +23,16 @@ from sqlalchemy.orm import (
     scoped_session,
 )
 
-
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+pg8000://username:passwd!@localhost/postgres"
-
-
+PG_CONN_URI_SYNC = "postgresql+pg8000://username:passwd!@localhost/postgres"
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://username:passwd!@localhost/postgres"
 
 
 
 
+Base = declarative_base()
 
-class Base:
+
+class Based():
     @declared_attr
     def __tablename__(cls):
         return f"{cls.__name__.lower()}s"
@@ -40,39 +40,51 @@ class Base:
     def __repr__(self):
         return str(self)
 
-engine = create_engine(url=PG_CONN_URI, echo=True)
-Base = declarative_base(bind=engine, cls=Base)
-
-session_factory = sessionmaker(bind=engine)
-Session = scoped_session(session_factory)
-
-#async_engine: AsyncEngine = create_async_engine(
-    #url=PG_CONN_URI,
-    #echo=True,
-#)
-#Base = declarative_base(bind=async_engine, cls=Base)
-
-#session_factory = sessionmaker(
-    #bind=async_engine,
-    #class_=AsyncSession,
-    #expire_on_commit=False,
-#)
-#Session=scoped_session(session_factory)
 
 class User(Base):
-    name = Column(String(20), unique=False, nullable=False, default="")
+    __tablename__="users"
+    id = Column(Integer, primary_key=True)
     username = Column(String(20), unique=True, nullable=False, default="")
+    name = Column(String(20), unique=False, nullable=False, default="")
     email = Column(String(64), unique=True, nullable=False, default="")
 
-    post = relationship("Post", back_populates="user", uselist=False)
+    post = relationship("Post", back_populates="user")
     def __str__(self):
         return f"{self.__class__.__name__}(" f"id={self.id}, " f"name={self.username!r}"
 
 class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
     title = Column(String(64), unique=False, nullable=False)
     body = Column(Text, nullable=False, default="")
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False,)
 
-    user = relationship("User", back_populates="posts", uselist=False)
+    user = relationship("User",back_populates="post")
     def __str__(self):
         return f"{self.__class__.__name__}(" f"id={self.id}, " f"name={self.title}"
+
+# sync
+#engine = create_engine(url=PG_CONN_URI_SYNC, echo=True)
+#Base = declarative_base(bind=engine, cls=Base)
+
+#session_factory = sessionmaker(bind=engine)
+#Session = scoped_session(session_factory)
+
+
+# async
+async_engine: AsyncEngine = create_async_engine(
+    url=PG_CONN_URI,
+    echo=True,
+)
+
+
+Session = sessionmaker(
+    async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+
+
+#Session=scoped_session(session_factory)
