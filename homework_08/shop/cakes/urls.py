c58a1efd6e297c23e5723d7a1f4b6affb606e671
ckamone <ckamone@gmail.com>
2022-12-22 23:01:48 +0300

    started to do hw 08

---
+++ homework_08/shop/cakes/urls.py
@@ -0,0 +1,30 @@
+"""shop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from cakes import views
+
+urlpatterns = [
+    path('ingredients/', views.IngredientListView.as_view(), name='ingredients_page'),
+    path('ingredients/create/', views.IngredientCreateView.as_view(), name='ingredients_create'),
+
+    path('categories/', views.CakeTypeListView.as_view(), name='categories_page'),
+    path('categories/create/', views.CakeTypeCreateView.as_view(), name='categories_create'),
+
+    path('cakes/<int:pk>', views.CakeDetailView.as_view(), name='cake_detail'),
+    path('cakes/', views.CakeListView.as_view(), name='cakes_page'),
+    path('cakes/create/', views.CakeCreateView.as_view(), name='cakes_create'),
+
+]
---
+++ homework_08/shop/cakes/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from cakes.models import Cake, CakeType, Ingredient
+from django.views.generic import (
+    ListView, DetailView, CreateView
+)
+# Create your views here.
+# views = controllers
+
+def main_page(request):
+    return render(request, 'cakes/index.html')
+
+#def categories_page(request):
+#    types = CakeType.objects.all()
+#    context = {
+#        'types': types
+#    }
+#    return render(request, 'cakes/categories.html', context=context)
+
+#def products_page(request):
+#    cakes = Cake.objects.all()
+#    context = {
+#        'cakes': cakes
+#    }
+#    return render(request, 'cakes/products.html', context=context)
+
+
+class CakeTypeListView(ListView):
+    model = CakeType
+
+
+class CakeTypeCreateView(CreateView):
+    model = CakeType
+    fields = '__all__'
+    success_url = reverse_lazy('categories_page')
+
+
+class IngredientListView(ListView):
+    model = Ingredient
+
+
+class IngredientCreateView(CreateView):
+    model = Ingredient
+    fields = '__all__'
+    success_url = reverse_lazy('ingredients_page')
+
+
+class CakeListView(ListView):
+    model = Cake
+
+
+class CakeDetailView(DetailView):
+    model = Cake
+
+
+class CakeCreateView(CreateView):
+    model = Cake
+    fields = '__all__'
+    success_url = reverse_lazy('cakes_page')
